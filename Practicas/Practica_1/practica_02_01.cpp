f62d101613dd850f07be13757c8d5ad82ae4eafe
jhoelgallegos-Imu <jhoel.gallegos@ucb.edu.bo>
2025-08-21 18:11:39 -0400

    On branch main 	new file:   Practicas/Practica_1/practica_02_01.cpp 	new file:   Practicas/Practica_1/practica_02_01.exe

---
+++ Practicas/Practica_1/practica_02_01.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int valor1=0;
+    int valor2=0;
+    cout<<"ingrese el valor 1: "<<endl;
+    cin >>valor1;
+    cout<<"ingrese el valor 2: "<<endl;
+    cin >>valor2;
+    if (valor1 < valor2)
+    {
+        cout<<valor1<<" es mayor que "<< valor2 <<endl;
+    }
+    else
+    {
+        if (valor1==valor2)
+        {
+            cout<<valor1<<" es igual que "<< valor2 <<endl;
+        }
+        else
+        {
+            cout<<valor1<<" es menor que "<< valor2 <<endl;
+        }
+    }
+    return 0 ;
+}
