e49370d54014b18dd4a58f972232f7770e702be0
jhoelgallegos-Imu <jhoel.gallegos@ucb.edu.bo>
2025-08-21 19:02:42 -0400

    modified:   hola.cpp 	modified:   hola.exe

--- hola.cpp
+++ hola.cpp
@@ -2,8 +2,9 @@
 using namespace std;
 
 int main() {
-    cout << "Hola, mundo!" << endl;
-    int x = 10 ;
+//cout << "Hola, mundo!" << endl;
+//    int edad=0;
+/*    int x = 10 ;
     cout << x ;
     int y = 6;
     int sum = x+y ;
@@ -21,6 +22,24 @@ int main() {
     double rad=10;
     double circ= 2*PI*rad;
     cout <<circ<<"cm. ";
-    cout<<"AYUDAAAAAA";
+    cout<<"AYUDAAAAAA";*/
+/*    do
+    {
+        cout<<"Ingrese su edad"<<endl;
+        cin>>edad;
+    } while (edad<0 || edad>=120 );
+    cout<<"ingreso la edad correctamente";*/
+    int numero=0;
+    cout<<"ingrese un numero n: "<<endl;
+    cin>>numero;
+    for (int i = 1; i <= numero; i++)
+    {
+        if (i%2 == 0) //si es par
+        {
+            cout<<i<<" - ";
+        }
+        
+    }
+    
     return 0;
 }
