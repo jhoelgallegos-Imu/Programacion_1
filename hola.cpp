d23b122d858a9ae3f5d43893f4d0f6efc8caca46
jhoelgallegos-Imu <jhoel.gallegos@ucb.edu.bo>
2025-08-21 18:09:29 -0400

    Initial commit 	new file:   Practicas/Practica_1/ejercicio_01_01.cpp 	new file:   Practicas/Practica_1/ejercicio_01_01.exe

---
+++ hola.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hola, mundo!" << endl;
+    int x = 10 ;
+    cout << x ;
+    int y = 6;
+    int sum = x+y ;
+    cout <<sum <<endl;
+    float deci=10.99;
+    cout <<deci <<endl;
+    char letra = 'J';
+    cout <<letra <<endl;
+    bool estudent=true;
+    bool on= false ;
+    std::string oracion= "Pepitoclavounclavito en la calva de un calvito";
+    cout<< "Un dia "<< oracion <<endl;
+    // constantes :b
+    const double PI=3.14159;
+    double rad=10;
+    double circ= 2*PI*rad;
+    cout <<circ<<"cm. ";
+    cout<<"AYUDAAAAAA";
+    return 0;
+}
