e522d20490152c6d69f32730c10ad3cd0aabe35f
jhoelgallegos-Imu <jhoel.gallegos@ucb.edu.bo>
2025-09-02 18:34:00 -0400

    modified:   Practicas/PRACTICA_01/Ejercicio_01_13.exe 	modified:   Practicas/PRACTICA_01/Ejercicio_01_25.exe 	modified:   hola.cpp     	modified:   hola.exe

--- hola.cpp
+++ hola.cpp
@@ -1,45 +1,8 @@
+// funciones: Dividir una tarea en chiquitas para hacerlo mejor :D
 #include <iostream>
 using namespace std;
 
 int main() {
-//cout << "Hola, mundo!" << endl;
-//    int edad=0;
-/*    int x = 10 ;
-    cout << x ;
-    int y = 6;
-    int sum = x+y ;
-    cout <<sum <<endl;
-    float deci=10.99;
-    cout <<deci <<endl;
-    char letra = 'J';
-    cout <<letra <<endl;
-    bool estudent=true;
-    bool on= false ;
-    std::string oracion= "Pepitoclavounclavito en la calva de un calvito";
-    cout<< "Un dia "<< oracion <<endl;
-    // constantes :b
-    const double PI=3.14159;
-    double rad=10;
-    double circ= 2*PI*rad;
-    cout <<circ<<"cm. ";
-    cout<<"AYUDAAAAAA";*/
-/*    do
-    {
-        cout<<"Ingrese su edad"<<endl;
-        cin>>edad;
-    } while (edad<0 || edad>=120 );
-    cout<<"ingreso la edad correctamente";*/
-    int numero=0;
-    cout<<"ingrese un numero n: "<<endl;
-    cin>>numero;
-    for (int i = 1; i <= numero; i++)
-    {
-        if (i%2 == 0) //si es par
-        {
-            cout<<i<<" - ";
-        }
-        
-    }
     
     return 0;
 }
