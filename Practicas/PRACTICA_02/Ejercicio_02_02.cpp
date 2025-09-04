7a1b207b9d3f65d61dd7287a0ebaaba75841f73f
jhoelgallegos-Imu <jhoel.gallegos@ucb.edu.bo>
2025-09-04 07:36:30 -0400

    new file:   Practicas/PRACTICA_02/Ejercicio_02_01.cpp 	new file:   Practicas/PRACTICA_02/Ejercicio_02_02.cpp 	new file:   Practicas/PRACTICA_02/Ejercicio_02_03.cpp 	new file:   Practicas/PRACTICA_02/Ejercicio_02_04.cpp 	new file:   Practicas/PRACTICA_02/Ejercicio_02_05.cpp 	new file:   Practicas/PRACTICA_02/Ejercicio_02_06.cpp 	new file:   Practicas/PRACTICA_02/Ejercicio_02_07.cpp

---
+++ Practicas/PRACTICA_02/Ejercicio_02_02.cpp
@@ -0,0 +1,6 @@
+// Materia: Programación I, Paralelo 4
+// Autor: Jhoel Fernando Gallegos Mamani
+// Carnet: 10904518 L.P.
+// Carrera del estudiante: Ingeniería en Multimedia e Interactividad
+// Fecha creación: 04/09/2025
+// Número de ejercicio: 2
