f647794e0fc82fe0db5bed232d8a44a1fca4e02c
jhoelgallegos-Imu <jhoel.gallegos@ucb.edu.bo>
2025-09-09 15:25:47 -0400

    modified:   Practicas/PRACTICA_02/Ejercicio_02_06.exe 	modified:   Practicas/PRACTICA_02/Ejercicio_02_07.cpp 	new file:   Practicas/PRACTICA_02/Ejercicio_02_07.exe

--- Practicas/PRACTICA_02/Ejercicio_02_07.cpp
+++ Practicas/PRACTICA_02/Ejercicio_02_07.cpp
@@ -2,5 +2,26 @@
 // Autor: Jhoel Fernando Gallegos Mamani
 // Carnet: 10904518 L.P.
 // Carrera del estudiante: Ingeniería en Multimedia e Interactividad
-// Fecha creación: 04/09/2025
+// Fecha creación: 09/09/2025
 // Número de ejercicio: 7
+#include <iostream>
+#include <ctime>
+using namespace std;
+
+int main() {
+    srand(time(NULL)); 
+    int respuesta = 0;
+    int ninnos1 = 0;
+    int ninnos2 = 0;
+    int ninnos3 = 0;
+    int ninnosT = 0;
+
+    cout << "Ingrese la cantidad de ninnos que hay en la guarderia: " << endl;
+    cin >> respuesta;
+    ninnos1 = rand() % (respuesta + 1); //Apartir de la generacion de los ninnos de 1 anno se general los demas para evitar que sobrepaso los que se establecio previamente              
+    ninnos2 = rand() % (respuesta - ninnos1 + 1);       
+    ninnos3 = respuesta - ninnos1 - ninnos2;           
+    ninnosT = (ninnos1 * 6) + (ninnos2 * 3) + (ninnos3 * 2);
+    cout << "La cantidad de panales usados en total seria: " << ninnosT << endl;
+    return 0;
+}
