95dfddbf8bf52bccf928d842cd9ef9eb98e20095
jhoelgallegos-Imu <jhoel.gallegos@ucb.edu.bo>
2025-09-09 15:04:42 -0400

    modified:   Practicas/PRACTICA_02/Ejercicio_02_06.cpp 	new file:   Practicas/PRACTICA_02/Ejercicio_02_06.exe

--- Practicas/PRACTICA_02/Ejercicio_02_06.cpp
+++ Practicas/PRACTICA_02/Ejercicio_02_06.cpp
@@ -4,3 +4,35 @@
 // Carrera del estudiante: Ingeniería en Multimedia e Interactividad
 // Fecha creación: 04/09/2025
 // Número de ejercicio: 6
+#include <iostream>
+#include <ctime>
+#include <cmath>
+using namespace std;
+
+int main() {
+    srand(time(NULL));
+    int respuesta = 0 ; 
+    int primos=0;
+    cout<<"Cuantos numeros aleatorios quiere generar? "<<endl;
+    cin>>respuesta;
+    for (int i = 1; i <= respuesta; i++)
+    {
+        int num = (rand() % 10000) + 1;
+        int primoscont = 0;
+        cout<<num<<" - ";
+        for (int j = 2; j <= sqrt(num); j++) // ayuda a que no sea tan lento al probar desde su raiz para que no sea tan tardado
+        {
+            if (num % j == 0)
+            {
+                primoscont+=1;
+                break;
+            }
+        }
+        if (primoscont==0 && num!=1)
+        {
+            primos+=1;
+        }
+    }
+    cout<<"\n Los numeros primos en total que salieron fueron "<<primos;
+    return 0;
+}
