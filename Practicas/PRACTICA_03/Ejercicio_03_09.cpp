16d84f33ae1889edb072ea7ce9e0617d581af4e7
jhoelgallegos-Imu <jhoel.gallegos@ucb.edu.bo>
2025-09-11 14:54:52 -0400

    modified:   Practicas/PRACTICA_03/Ejercicio_03_09.cpp 	new file:   Practicas/PRACTICA_03/Ejercicio_03_09.exe 	modified:   Practicas/PRACTICA_03/Ejercicio_03_10.cpp 	new file:   Practicas/PRACTICA_03/Ejercicio_03_10.exe

--- Practicas/PRACTICA_03/Ejercicio_03_09.cpp
+++ Practicas/PRACTICA_03/Ejercicio_03_09.cpp
@@ -5,7 +5,37 @@
 #include <iostream>
 using namespace std;
 
+int horasextra = 0;
+float convercion(float t, int &horasextra);
+
 int main () {
-    
+    float time = 0;
+    cout << "Cuanto tiempo ha trabajado (Ingrese en minutos):" << endl;
+    cin >> time;
+
+    float horas = convercion(time, horasextra);
+
+    if (horas > 8) {
+        cout << "Usted trabajo 8 horas con " << horasextra << " de horas extra." << endl;
+    }
+    else if (horas < 8) {
+        float resto = 8 - horas;
+        cout << "Usted no completo su jornada laboral, solo hizo " << horas 
+             << " horas. Le debe a la empresa " << resto << " horas." << endl;
+    }
+    else {
+        cout << "Felicidades, usted completo su jornada de 8 horas :D" << endl;
+    }
+
     return 0;
+}
+
+float convercion(float t, int &horasextra) { //las horas extra enlasadas hace que sea mas facil
+    float horas = t / 60;
+    if (horas > 8) {
+        horasextra = horas - 8;
+    } else {
+        horasextra = 0;
+    }
+    return horas;
 }
--- Practicas/PRACTICA_03/Ejercicio_03_10.cpp
+++ Practicas/PRACTICA_03/Ejercicio_03_10.cpp
@@ -5,7 +5,22 @@
 #include <iostream>
 using namespace std;
 
+double calcularTarifa(double kmR);
+
 int main () {
-    
+    double km=0;
+    cout<<"Ingrese los kilometros recorrigos"<<endl;
+    cin>>km;
+    cout<<"La tarifa total es de: "<<calcularTarifa(km)<<endl;
     return 0;
+}
+
+double calcularTarifa(double kmR){
+    int tarifa=0;
+    tarifa= (kmR*2)+10;
+    if (kmR>10)
+    {
+        tarifa-=(tarifa*0.10);
+    }
+    return tarifa;
 }
