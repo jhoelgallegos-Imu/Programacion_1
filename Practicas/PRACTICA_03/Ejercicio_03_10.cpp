de4a1495d93dc29c4a760f39434879afb472655f
jhoelgallegos-Imu <jhoel.gallegos@ucb.edu.bo>
2025-09-11 11:54:47 -0400

    modified:   Practicas/PRACTICA_03/Ejercicio_03_04.cpp 	modified:   Practicas/PRACTICA_03/Ejercicio_03_05.cpp 	modified:   Practicas/PRACTICA_03/Ejercicio_03_06.cpp 	modified:   Practicas/PRACTICA_03/Ejercicio_03_07.cpp 	modified:   Practicas/PRACTICA_03/Ejercicio_03_08.cpp 	modified:   Practicas/PRACTICA_03/Ejercicio_03_09.cpp 	modified:   Practicas/PRACTICA_03/Ejercicio_03_10.cpp 	modified:   Practicas/PRACTICA_03/Ejercicio_03_11.cpp

--- Practicas/PRACTICA_03/Ejercicio_03_10.cpp
+++ Practicas/PRACTICA_03/Ejercicio_03_10.cpp
@@ -2,3 +2,10 @@
 // Autor:Jhoel Fernando Gallegos Mamani
 // Fecha creación: 11/09/2025
 // Número de ejercicio: 10
+#include <iostream>
+using namespace std;
+
+int main () {
+    
+    return 0;
+}
