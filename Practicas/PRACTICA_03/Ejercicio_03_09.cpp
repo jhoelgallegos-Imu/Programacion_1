2dc0a095e7a7bb3d86ad7f50b127309efdf3f6dc
jhoelgallegos-Imu <jhoel.gallegos@ucb.edu.bo>
2025-09-11 11:15:30 -0400

    new file:   Practicas/PRACTICA_03/Ejercicio_03_01.cpp 	new file:   Practicas/PRACTICA_03/Ejercicio_03_02.cpp 	new file:   Practicas/PRACTICA_03/Ejercicio_03_03.cpp 	new file:   Practicas/PRACTICA_03/Ejercicio_03_04.cpp 	new file:   Practicas/PRACTICA_03/Ejercicio_03_05.cpp 	new file:   Practicas/PRACTICA_03/Ejercicio_03_06.cpp 	new file:   Practicas/PRACTICA_03/Ejercicio_03_07.cpp 	new file:   Practicas/PRACTICA_03/Ejercicio_03_08.cpp 	new file:   Practicas/PRACTICA_03/Ejercicio_03_09.cpp 	new file:   Practicas/PRACTICA_03/Ejercicio_03_10.cpp 	new file:   Practicas/PRACTICA_03/Ejercicio_03_11.cpp 	modified:   hola.cpp

---
+++ Practicas/PRACTICA_03/Ejercicio_03_09.cpp
@@ -0,0 +1,4 @@
+// Materia: Programación I, Paralelo 4
+// Autor:Jhoel Fernando Gallegos Mamani
+// Fecha creación: 11/09/2025
+// Número de ejercicio: 9
--- hola.cpp
+++ hola.cpp
@@ -3,6 +3,10 @@
 using namespace std;
 
 int main() {
-    
+    int bolivianos=0;
+    cout<<"Ingrese la cantidad de bolivianos que tiene"<<endl;
+    cin>>bolivianos;
+
     return 0;
 }
+
