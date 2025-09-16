ac9417e52f506006bfd1e21944a89201d91879d9
jhoelgallegos-Imu <jhoel.gallegos@ucb.edu.bo>
2025-09-16 16:17:21 -0400

    modified:   Practicas/PRACTICA_04/Ejercicio_04_04.cpp 	new file:   Practicas/PRACTICA_04/Ejercicio_04_04.exe 	modified:   Practicas/PRACTICA_04/Ejercicio_04_05.cpp 	new file:   Practicas/PRACTICA_04/Ejercicio_04_05.exe

--- Practicas/PRACTICA_04/Ejercicio_04_04.cpp
+++ Practicas/PRACTICA_04/Ejercicio_04_04.cpp
@@ -1,10 +1,43 @@
 // Materia: Programación I, Paralelo 4
 // Autor: Jhoel Fernando Gallegos Mamani
-// Fecha creación: 15/09/2025
+// Fecha creación: 16/09/2025
 // Número de ejercicio: 4
 #include <iostream>
 using namespace std;
 
+double ConvertirCelciusAFahrenheit(double celcius);
+double MayorTemperatura(double a, double b);
 int main () {
+    double c=0,x,y;
+    cout<<"Ingrese la cantidad para hacer la conversion: "<<endl;
+    cin>>c;
+    cout<<"La cantidad convertida es igual a: "<<ConvertirCelciusAFahrenheit(c)<<endl;
+    cout<<"Ingrese una cantidad para compararla: "<<endl;
+    cin>>x;
+    cout<<"Ingrese la cantidad a comparar: "<<endl;
+    cin>>y;
+    cout<<"El mayor es: ";
+    if (MayorTemperatura(x,y)==1)
+    {
+        cout<<x;
+    }
+    else
+    {
+        cout<<y;
+    }
     return 0;
+}
+double ConvertirCelciusAFahrenheit(double celcius){
+    celcius=(celcius*9/5)+32;
+    return celcius;
+}
+double MayorTemperatura(double a, double b) {
+    if (a>b) //dependiendo de cual sea mayor regresara un numero o otro
+    {
+        return 1;
+    }
+    else
+    {
+        return 18;
+    }
 }
--- Practicas/PRACTICA_04/Ejercicio_04_05.cpp
+++ Practicas/PRACTICA_04/Ejercicio_04_05.cpp
@@ -1,10 +1,35 @@
 // Materia: Programación I, Paralelo 4
 // Autor: Jhoel Fernando Gallegos Mamani
-// Fecha creación: 15/09/2025
+// Fecha creación: 16/09/2025
 // Número de ejercicio: 5
 #include <iostream>
 using namespace std;
 
+const float salario=6.189;
+float tarifa(int time);
+float extra(int tiempo);
 int main () {
+    int horas=0;
+    cout<<"Ingrese las horas que haya trabajado: "<<endl;
+    cin>>horas;
+    cout<<"La tarifa total por sus horas de trabajo es de: "<<tarifa(horas)<<endl;
+    if (horas>8)
+    {
+        cout<<"La bonificacion sera de: "<<extra(horas);
+    }
+    
     return 0;
+}
+float tarifa(int time){
+    float money= time*salario;
+    if (time>8)
+    {
+        money=8*salario;
+    }
+    return money;
+}
+float extra(int tiempo){
+    tiempo-=8; //para solo calcular el tiempo extra y no todo el tiempo
+    float money=tiempo*salario;
+    return money;
 }
