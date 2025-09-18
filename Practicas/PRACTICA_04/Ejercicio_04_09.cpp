a2f73bb096e7ea64cb763e16909efab67119c7f6
jhoelgallegos-Imu <jhoel.gallegos@ucb.edu.bo>
2025-09-18 11:47:20 -0400

    modified:   Practicas/PRACTICA_04/Ejercicio_04_09.cpp 	new file:   Practicas/PRACTICA_04/Ejercicio_04_09.exe

--- Practicas/PRACTICA_04/Ejercicio_04_09.cpp
+++ Practicas/PRACTICA_04/Ejercicio_04_09.cpp
@@ -11,15 +11,37 @@ using namespace std;
 int main () {
     srand(time(0));
     int n=0, antiguedad=0;
-    float comision=0;
+    float comision=0, Precomision=0 ,sueldo=0;
     cout<<"Cuantas vendedoras tiene la tienda de cosmeticos? "<<endl;
     cin>>n;
     for (int i = 1; i <= n; i++)
     {
-        antiguedad=(rand());
-        int ProdVend=(rand() % 10);
+        antiguedad=(rand()%3)+1; // la antiguedad como no esta definida la de 1 a 3 annos
+        switch (antiguedad)
+        {
+        case 1:
+            sueldo+=2000;
+            break;
+        case 2:
+            sueldo+=2500;
+            break;
+        case 3:
+            sueldo+=3000;
+            break;
+        
+        default:
+            break;
+        }
+        int ProdVend=(rand() % 10)+1;
+        for (int j = 1; j < ProdVend; j++)
+        {
+            float PrecProdc=(rand()%81)+20; //rango de precio de productos vendidos
+            Precomision+=PrecProdc;
+        }
     }
-    
-    
+    comision=Precomision*0.10;
+    sueldo+=comision;
+    cout<<"Las comisiones en total serian igual a: "<<comision<<endl;
+    cout<<"El sueldo total a pagar a "<<n<<" vendedoras/vendedores seria: "<<sueldo<<endl;
     return 0;
 }
