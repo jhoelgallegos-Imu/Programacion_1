5ab64cb8180fd578193a59d8e83660cf79a89559
jhoelgallegos-Imu <jhoel.gallegos@ucb.edu.bo>
2025-09-17 23:33:03 -0400

    modified:   Practicas/PRACTICA_04/Ejercicio_04_08.cpp 	new file:   Practicas/PRACTICA_04/Ejercicio_04_08.exe 	modified:   Practicas/PRACTICA_04/Ejercicio_04_09.cpp

--- Practicas/PRACTICA_04/Ejercicio_04_08.cpp
+++ Practicas/PRACTICA_04/Ejercicio_04_08.cpp
@@ -3,8 +3,29 @@
 // Fecha creación: 15/09/2025
 // Número de ejercicio: 8
 #include <iostream>
+#include <cstdlib>  
+#include <ctime>
 using namespace std;
 
 int main () {
+    srand(time(0)); 
+    int n=0;
+    float IVA=0;
+    float total=0;
+    cout<<"Ingrese la cantidad de productos que quiera comprar"<<endl;
+    cin>>n;
+    for (int i = 1; i <= n; i++)
+    {
+        float producto=(rand() % 31)+20; //se generan los productos en un rango de 20 a 50
+        total += producto * 1.13;
+        IVA+=producto*0.13;
+    }
+    cout<<"El total seria: "<<total<<endl;
+    if (total>2500)
+    {
+        total = total * 0.95;
+        cout<<"El total debido a la compra por mayor se reducira en 5% y ahora es: "<<total<<endl;
+    }
+    cout<<"El costo total al IVA seria: "<<IVA;
     return 0;
 }
--- Practicas/PRACTICA_04/Ejercicio_04_09.cpp
+++ Practicas/PRACTICA_04/Ejercicio_04_09.cpp
@@ -4,8 +4,22 @@
 // Número de ejercicio: 9
 
 #include <iostream>
+#include <cstdlib>  
+#include <ctime>
 using namespace std;
 
 int main () {
+    srand(time(0));
+    int n=0, antiguedad=0;
+    float comision=0;
+    cout<<"Cuantas vendedoras tiene la tienda de cosmeticos? "<<endl;
+    cin>>n;
+    for (int i = 1; i <= n; i++)
+    {
+        antiguedad=(rand());
+        int ProdVend=(rand() % 10);
+    }
+    
+    
     return 0;
 }
