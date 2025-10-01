c7daf62d046ec93fac546147791ca6e2dd2e83ac
jhoelgallegos-Imu <jhoel.gallegos@ucb.edu.bo>
2025-10-01 17:20:10 -0400

    modified:   Practicas/PRACTICA_05/Ejercicio_05_01.cpp 	modified:   Practicas/PRACTICA_05/Ejrcicio_05_02.cpp 	modified:   Practicas/PRACTICA_05/Ejrcicio_05_03.cpp 	modified:   Practicas/PRACTICA_05/Ejrcicio_05_04.cpp 	modified:   Practicas/PRACTICA_05/Ejrcicio_05_05.cpp 	modified:   Practicas/PRACTICA_05/Ejrcicio_05_06.cpp 	modified:   Practicas/PRACTICA_05/Ejrcicio_05_07.cpp

--- Practicas/PRACTICA_05/Ejrcicio_05_07.cpp
+++ Practicas/PRACTICA_05/Ejrcicio_05_07.cpp
@@ -4,3 +4,11 @@
 // Carrera del estudiante: Ingeniería de Multimedia e Interactividad
 // Fecha creación: 01/10/2025
 // Número de ejercicio: 7
+#include <iostream>
+#include <cstdlib>  
+#include <ctime>
+using namespace std;
+
+int main () {
+    return 0;
+}
