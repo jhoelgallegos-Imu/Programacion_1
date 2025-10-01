9c4583e8e10a84354b59332a4289c679f2b00b1c
jhoelgallegos-Imu <jhoel.gallegos@ucb.edu.bo>
2025-10-01 17:16:40 -0400

    modified:   Practicas/PRACTICA_03/Ejercicio_03_11.exe 	modified:   Practicas/PRACTICA_04/Ejercicio_04_06.exe 	new file:   Practicas/PRACTICA_05/Ejercicio_05_01.cpp 	new file:   Practicas/PRACTICA_05/Ejrcicio_05_02.cpp 	new file:   Practicas/PRACTICA_05/Ejrcicio_05_03.cpp 	new file:   Practicas/PRACTICA_05/Ejrcicio_05_04.cpp 	new file:   Practicas/PRACTICA_05/Ejrcicio_05_05.cpp 	new file:   Practicas/PRACTICA_05/Ejrcicio_05_06.cpp 	new file:   Practicas/PRACTICA_05/Ejrcicio_05_07.cpp 	modified:   hola.cpp 	modified:   hola.exe

---
+++ Practicas/PRACTICA_05/Ejrcicio_05_07.cpp
@@ -0,0 +1,6 @@
+// Materia: Programación I, Paralelo 4
+// Autor:Jhoel Fernando Gallegos Mamani
+// Carnet: 10904518
+// Carrera del estudiante: Ingeniería de Multimedia e Interactividad
+// Fecha creación: 01/10/2025
+// Número de ejercicio: 7
--- hola.cpp
+++ hola.cpp
@@ -1,12 +1,29 @@
 // funciones: Dividir una tarea en chiquitas para hacerlo mejor :D
 #include <iostream>
+#include <vector>
+#include <ctime>
+#include <cstdlib>
 using namespace std;
 
-int main() {
-    int bolivianos=0;
-    cout<<"Ingrese la cantidad de bolivianos que tiene"<<endl;
-    cin>>bolivianos;
+vector<int> cienannos() {
+    vector<int> annos;
+    int rango = 2025 - 1990;
+    for (int i = 0; i < 100; i++) {
+        double aleatorionormalizado = (double)rand() / RAND_MAX;
+        int anno = 1990 + int(aleatorionormalizado * (rango + 1));
+        annos.push_back(anno);
+    }
+    return annos;
+}
 
+
+int main() {
+    vector<int> listaannos= cienannos();
+    for (int i = 0; i <=99; i++)
+    {
+        cout<<i+1<<" "<<listaannos[i]<<endl;
+    }
+    
     return 0;
 }
 
