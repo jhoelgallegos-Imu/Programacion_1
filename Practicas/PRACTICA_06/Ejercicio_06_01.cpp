f5f2a90e7c213ac996346d9a0ca1125d2b016b67
jhoelgallegos-Imu <jhoel.gallegos@ucb.edu.bo>
2025-10-07 17:17:17 -0400

    new file:   Practicas/PRACTICA_06/Ejercicio_06_01.cpp 	new file:   Practicas/PRACTICA_06/Ejercicio_06_02.cpp 	new file:   Practicas/PRACTICA_06/Ejercicio_06_03.cpp 	new file:   Practicas/PRACTICA_06/Ejercicio_06_04.cpp 	new file:   Practicas/PRACTICA_06/Ejercicio_06_05.cpp 	new file:   Practicas/PRACTICA_06/Ejercicio_06_06.cpp 	new file:   Practicas/PRACTICA_06/Ejercicio_06_07.cpp 	new file:   Practicas/PRACTICA_06/Ejercicio_06_08.cpp 	new file:   Practicas/PRACTICA_06/Ejercicio_06_09.cpp 	new file:   Practicas/PRACTICA_06/Ejercicio_06_10.cpp 	new file:   Practicas/PRACTICA_06/Ejercicio_06_11.cpp 	new file:   Practicas/PRACTICA_06/Ejercicio_06_12.cpp 	new file:   Practicas/PRACTICA_06/Ejercicio_06_13.cpp 	new file:   Practicas/PRACTICA_06/Ejercicio_06_14.cpp 	modified:   hola.cpp

---
+++ Practicas/PRACTICA_06/Ejercicio_06_01.cpp
@@ -0,0 +1,13 @@
+// Materia: Programación I, Paralelo 1
+// Autor: Jhoel Fernando Gallegos Mamani
+// Fecha creación: 7/10/2024
+// Número de ejercicio: 1
+// Problema planteado: 
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+int main () {
+    return 0;
+}
