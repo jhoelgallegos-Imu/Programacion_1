81fc198b9150a066391435ca59a2824696124a8c
jhoelgallegos-Imu <jhoel.gallegos@ucb.edu.bo>
2025-10-08 20:04:48 -0400

    modified:   Practicas/PRACTICA_06/Ejercicio_06_01.cpp 	modified:   hola.cpp

--- Practicas/PRACTICA_06/Ejercicio_06_01.cpp
+++ Practicas/PRACTICA_06/Ejercicio_06_01.cpp
@@ -2,12 +2,47 @@
 // Autor: Jhoel Fernando Gallegos Mamani
 // Fecha creación: 7/10/2024
 // Número de ejercicio: 1
-// Problema planteado: 
+// Problema planteado: Almacene en un arreglo las edades de varias personas (hasta ingresar un -1) y a partir
+// de este arreglo determine la desviación típica.
 #include <iostream>
 #include <vector>
 #include <string>
+#include <cmath>
 using namespace std;
 
+float DesviacionTipica(vector<int> edades, int tam, int prom){
+    float DesTip=0;
+    for (int i = 0; i < tam; i++)
+    {
+        DesTip+=pow(edades[i]-prom, 2);
+    }
+    DesTip/=tam;
+    DesTip=sqrt(DesTip);
+    return DesTip;
+}
 int main () {
+    vector<int> edades;
+    int n, prom=0, tam;
+    do
+    {
+        cout<<"Ingrese una edad, si quiere dejar de ingresar -1 "<<endl;
+        cin>>n;
+        if (n!=-1)
+        {
+            edades.push_back(n);
+        }
+    } while (n!=-1);
+    tam=edades.size();
+    for (int i = 0; i < edades.size(); i++)
+    {
+        cout<<edades[i]<<"   ";
+    }
+    cout<<endl;
+    for (int i = 0; i < edades.size(); i++)
+    {
+        prom+=edades[i];
+    }
+    prom/=tam;
+    cout<<"La desviacion tipica de las edades es igual a: "<<DesviacionTipica(edades, tam, prom);
     return 0;
 }
--- hola.cpp
+++ hola.cpp
@@ -3,8 +3,32 @@
 #include <ctime>
 #include <cstdlib>
 using namespace std;
+//matrices
 
 int main() {
+    srand(time(0));
+    int edades[4][5];
+    for (int i = 0; i < 3; i++) //
+    {
+        for (int j = 0; j < 3; j++) //columnas
+        {
+            
+            if (i==j)
+            {
+                edades[i][j]=-10;
+            }
+            else
+            {
+                edades[i][j]=(rand() % 100)+1;
+            }
+            
+            
+            cout<<edades[i][j]<<" \t";
+
+        }
+        cout<<endl;
+    }
+    
     return 0;
 }
 
