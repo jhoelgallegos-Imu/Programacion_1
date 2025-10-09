64a3ce7db2cd06eeb37c3615d8fc213348cba428
jhoelgallegos-Imu <jhoel.gallegos@ucb.edu.bo>
2025-10-09 13:00:52 -0400

    modified:   Practicas/PRACTICA_06/Ejercicio_06_10.cpp

--- Practicas/PRACTICA_06/Ejercicio_06_10.cpp
+++ Practicas/PRACTICA_06/Ejercicio_06_10.cpp
@@ -1,13 +1,33 @@
 // Materia: Programación I, Paralelo 1
 // Autor: Jhoel Fernando Gallegos Mamani
-// Fecha creación: 7/10/2024
+// Fecha creación: 9/10/2024
 // Número de ejercicio: 10
-// Problema planteado: 
+/* Problema planteado: Escribe un programa que elimine todos los dígitos de una cadena.
+                 Entrada: "abc123def456"
+                 Salida: "abcdef"                       */ 
+
 #include <iostream>
 #include <vector>
 #include <string>
 using namespace std;
 
+void EliminarDigitos(string p){
+    string nuevo;
+    for (int i = 0; i < p.size(); i++)
+    {
+        if (!(p[i] >= '0' && p[i] <= '9')) 
+        {
+            nuevo += p[i]; 
+        }
+    }
+    cout<<"La cadena sin digitos es: "<<nuevo;
+}
+
 int main () {
+    string alfanumericos="";
+    cout<<"Ingrese la combinacion de alfanumericos para eliminar los digitos: "<<endl;
+    getline(cin, alfanumericos);
+    cout<<"-----------------------------------------------------------------"<<endl;
+    EliminarDigitos(alfanumericos);
     return 0;
 }
