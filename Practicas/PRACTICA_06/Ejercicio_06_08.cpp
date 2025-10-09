ea9f67fa43c068d78c73135f3cfab4bcabeffac5
jhoelgallegos-Imu <jhoel.gallegos@ucb.edu.bo>
2025-10-09 12:34:35 -0400

    modified:   Practicas/PRACTICA_06/Ejercicio_06_08.cpp

--- Practicas/PRACTICA_06/Ejercicio_06_08.cpp
+++ Practicas/PRACTICA_06/Ejercicio_06_08.cpp
@@ -1,13 +1,51 @@
 // Materia: Programación I, Paralelo 1
 // Autor: Jhoel Fernando Gallegos Mamani
-// Fecha creación: 7/10/2024
+// Fecha creación: 9/10/2024
 // Número de ejercicio: 8
-// Problema planteado: 
+/* Problema planteado: Escribe un programa que valide si una cadena ingresada cumple con el formato básico
+de una dirección de correo electrónico (por ejemplo, contiene exactamente un '@' y al
+menos un punto '.' después del '@').
+• // Entrada: "usuario@example.com"
+• // Salida: "Correo electrónico válido"
+• // Entrada: "usuarioexample.com"
+• // Salida: "Correo electrónico inválido"  */
+
 #include <iostream>
 #include <vector>
 #include <string>
 using namespace std;
 
+void ValidarCorreo(string v){
+    bool valid = false;
+    for (int i = 0; i < v.size(); i++)
+    {
+        if (v[i]=='@')//busca un arroba
+        {
+            for (int j = i; j < v.size() ; j++) //verifica un punto despues del arroba 
+            {
+                if (v[j]=='.')
+                {
+                    valid=true; //al verificar ambos se vuelve verdadero
+                }
+            }
+        }
+    }
+    if (valid)
+    {
+        cout<<"Su correo es valido :D ";
+    }
+    else
+    {
+        cout<<"Su correo no es valido D: ";
+    }
+    
+}
+
 int main () {
+    string correo = "";
+    cout<<"Ingrese su correo para validarlo: "<<endl;
+    getline(cin, correo);
+    cout<<"-------------------------------------------------"<<endl;
+    ValidarCorreo(correo);
     return 0;
 }
