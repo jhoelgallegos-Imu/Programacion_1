f5ab2c7e6f9306e3d5b080235d7abc8b91ced196
jhoelgallegos-Imu <jhoel.gallegos@ucb.edu.bo>
2025-10-18 15:20:28 -0400

    modified:   Practicas/PRACTICA_07/Ejercicio_07_02.cpp 	modified:   Practicas/PRACTICA_07/Ejercicio_07_03.cpp

--- Practicas/PRACTICA_07/Ejercicio_07_02.cpp
+++ Practicas/PRACTICA_07/Ejercicio_07_02.cpp
@@ -2,13 +2,87 @@
 // Autor: Jhoel Fernando Gallegos Mamani
 // Carnet: 10904518
 // Carrera del estudiante: Ingeniería de Multimedia e Interactividad
-// Fecha creación: 14/10/2025
+// Fecha creación: 18/10/2025
 // Número de ejercicio: 2
 
 #include <iostream>
 #include <vector>
+#include <cstdlib>
+#include <ctime>
+#include <cmath>
 using namespace std;
 
+int GRandom(int p, int f){
+    return rand() % (f - p + 1) + p;
+}
+
+int sumaUltimaColumna(const vector<vector<int>>& m, int n){
+    int suma = 0;
+    for (int i = 0; i < n; i++){ //recorre la ultima columna y suma sus digitos
+        suma += m[i][n - 1];
+    }
+    return suma;
+}
+
+int productoUltimaFila(const vector<vector<int>>& m, int n){
+    int producto = 1; //se empieza en 1 pq si empezara en 0 todo terminaria siendo 0
+    for (int j = 0; j < n; j++){ //recorre la ultima fila y saca el producto de sus digitos
+        producto *= m[n - 1][j];
+    }
+    return producto;
+}
+
+float desviacionEstandar(const vector<vector<int>>& m, int n){
+    float suma = 0;
+    int total = n * n;
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < n; j++){
+            suma += m[i][j];
+        }
+    }
+    float media = suma / total;
+    float sumaCuadrados = 0;
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < n; j++){
+            sumaCuadrados += pow(m[i][j] - media, 2);
+        }
+    }
+    float DE = sqrt(sumaCuadrados / total);
+    return DE;
+}
+
 int main(){
+    srand(time(0));
+    int n, a, b;
+    cout << "Ingrese el tamano de la matriz cuadrada: ";
+    cin >> n;
+    cout << "Ingrese el limite inferior: ";
+    cin >> a;
+    cout << "Ingrese el limite superior: ";
+    cin >> b;
+    vector<vector<int>> matriz(n, vector<int>(n));
+    int Vmayor = a, pi = 0, pj = 0;
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < n; j++){
+            matriz[i][j] = GRandom(a, b);
+            if (matriz[i][j] > Vmayor){
+                Vmayor = matriz[i][j];
+                pi = i + 1;
+                pj = j + 1;
+            }
+        }
+    }
+    cout << "\nMatriz generada:\n";
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < n; j++){
+            cout << matriz[i][j] << "\t";
+        }
+        cout << endl;
+    }
+    cout << "\nLa suma de la ultima columna es: " << sumaUltimaColumna(matriz, n) << endl;
+    cout << "El producto total de la ultima fila es: " << productoUltimaFila(matriz, n) << endl;
+    cout << "El valor mas grande en la matriz es: " << Vmayor
+         << " que esta en: (" << pi << "," << pj << ")" << endl;
+    cout << "La desviacion estandar de la matriz es: " << desviacionEstandar(matriz, n) << endl;
     return 0;
 }
--- Practicas/PRACTICA_07/Ejercicio_07_03.cpp
+++ Practicas/PRACTICA_07/Ejercicio_07_03.cpp
@@ -2,13 +2,120 @@
 // Autor: Jhoel Fernando Gallegos Mamani
 // Carnet: 10904518
 // Carrera del estudiante: Ingeniería de Multimedia e Interactividad
-// Fecha creación: 14/10/2025
+// Fecha creación: 18/10/2025
 // Número de ejercicio: 3
 
 #include <iostream>
 #include <vector>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 
+int GRandom(int p, int f){
+    return rand() % (f - p + 1) + p;
+}
+
+int FilasLibres(vector<vector<char>> m, int f, int c ){ //escanea las filas y si detecta un zombie ya no la cuenta
+    int FL=0;
+    for (int i = 0; i < f; i++)
+    {
+        bool confirmacion=true;
+        for (int j = 0; j < c; j++)
+        {
+            if (m[i][j]=='x')
+            {
+                confirmacion= false;
+                break;
+            }
+        }
+        if (confirmacion==true)
+        {
+            FL++;
+        }
+    }
+    return FL;
+}
+
+int ColumnasLibres(vector<vector<char>> m, int f, int c ){ //escanea las columnas y si detecta un zombie ya no la cuenta
+    int CL=0;
+    for (int j = 0; j < c; j++)
+    {
+        bool confirmacion = true;
+        for (int i = 0; i < f; i++)
+        {
+            if (m[i][j]=='x')
+            {
+                confirmacion= false;
+                break;
+            }
+        }
+        if (confirmacion==true)
+        {
+            CL++;
+        }
+    }
+    return CL;
+}
+
+bool SePuedeEntrar(vector<vector<char>> m, int f, int c){ //escanea la 1 fila en busca de zombies
+    int zombies=0;
+    for (int i = 0; i < c ; i++)
+    {
+        if (m[0][i]=='x')
+        {
+            zombies++;
+        }
+    }
+    if (zombies>=2)
+    {
+        return false;
+    }
+    return true;
+}
+
 int main(){
+    srand(time(0));
+    int filas=GRandom(4,8) , columnas= GRandom(4,8), ZombiesTotales=0;
+    vector<vector<char>> MapaDePrision(filas);
+    vector<int> coordenadasI;
+    vector<int> coordenadasJ;
+    cout<<"\t Mapa de la Prision: \t"<<endl;
+    for (int i = 0; i < filas; i++) {
+        for (int j = 0; j < columnas; j++) {
+            int ZoL = GRandom(1, 10);
+            if (ZoL >= 5)
+                MapaDePrision[i].push_back('o'); 
+            else
+            {
+                MapaDePrision[i].push_back('x');
+                coordenadasI.push_back(i+1);
+                coordenadasJ.push_back(j+1);
+                ZombiesTotales++;
+            }
+        }
+    }
+    for (int i = 0; i < filas; i++) {
+        for (int j = 0; j < columnas; j++) {
+            cout << MapaDePrision[i][j] << " ";
+        }
+        cout << endl;
+    }
+    cout<<"b) las filas sin zombies son: "<<FilasLibres(MapaDePrision, filas, columnas)
+    <<" las columnas sin zombies son: "<<ColumnasLibres(MapaDePrision, filas, columnas)<<endl
+    <<"c) Las coordenadas de los muertos vivientes son: "<<endl;
+    for (int i = 0; i < coordenadasI.size(); i++)
+    {
+        cout<<"zombie "<<i+1<<" esta en ("<<coordenadasI[i]<<","<<coordenadasJ[i]<<")"<<endl;
+    }
+    cout<<"d) La cantidad total de zombies en toda la matriz es: "<<ZombiesTotales<<endl;
+    if (SePuedeEntrar(MapaDePrision, filas, columnas)==false)
+    {
+        cout<<"e) No es posible entrar en la instalacion :( ";
+    }
+    else
+    {
+        cout<<"e) Es posible entrar en la instalacion :D ";
+    }
+    
     return 0;
 }
