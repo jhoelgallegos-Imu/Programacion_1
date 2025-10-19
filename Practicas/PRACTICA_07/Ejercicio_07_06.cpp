7fce4ba2a71164d1edc943b2146ea7f4f4681a4b
jhoelgallegos-Imu <jhoel.gallegos@ucb.edu.bo>
2025-10-18 21:37:47 -0400

    modified:   Practicas/PRACTICA_07/Ejercicio_07_06.cpp

--- Practicas/PRACTICA_07/Ejercicio_07_06.cpp
+++ Practicas/PRACTICA_07/Ejercicio_07_06.cpp
@@ -2,13 +2,45 @@
 // Autor: Jhoel Fernando Gallegos Mamani
 // Carnet: 10904518
 // Carrera del estudiante: Ingeniería de Multimedia e Interactividad
-// Fecha creación: 14/10/2025
+// Fecha creación: 18/10/2025
 // Número de ejercicio: 6
 
 #include <iostream>
 #include <vector>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 
+int GRandom(int p, int f){
+    return rand() % (f - p + 1) + p;
+}
+
+void MostrarTranspuesta(vector<vector<int>> m, int f, int c){ //esto solo muestra su transpuesta ya que no se pidio alguna operacion con esta
+    for (int j = 0; j < c; j++)
+    {
+        for (int i = 0; i < f; i++)
+        {
+            cout<<"\t"<<m[i][j];
+        }
+        cout<<endl;
+    }
+}
+
 int main(){
+    srand(time(0));
+    int f = GRandom(1,4), c = GRandom(2,4);
+    vector<vector<int>> matriz(f,vector<int>(c));
+    cout<<"\t Matriz original: \t"<<endl<<" ============================================ "<<endl;
+    for (int i = 0; i < f; i++)
+    {
+        for (int j = 0; j < c ; j++)
+        {
+            matriz[i][j]=GRandom(1,10);
+            cout<<"\t"<<matriz[i][j];
+        }
+        cout<<endl;
+    }
+    cout<<endl<<"\t Matriz Transpuesta: \t"<<endl<<" ============================================ "<<endl;
+    MostrarTranspuesta(matriz, f, c);
     return 0;
 }
