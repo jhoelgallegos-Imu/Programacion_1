f3c9e3ae23f83bd727bf9ce6a8cf00f39a610b3c
jhoelgallegos-Imu <jhoel.gallegos@ucb.edu.bo>
2025-11-02 17:08:43 -0400

    new file:   Practicas/PRACTICA_09/Practica_09_01.cpp 	new file:   Practicas/PRACTICA_09/Practica_09_02.cpp 	new file:   Practicas/PRACTICA_09/Practica_09_03.cpp 	new file:   Practicas/PRACTICA_09/Practica_09_04.cpp 	new file:   Practicas/PRACTICA_09/Practica_09_05.cpp 	new file:   Practicas/PRACTICA_09/Practica_09_06.cpp 	new file:   Practicas/PRACTICA_09/Practica_09_07.cpp 	new file:   Practicas/PRACTICA_09/Practica_09_08.cpp 	new file:   Practicas/PRACTICA_09/Practica_09_09.cpp 	new file:   Practicas/PRACTICA_09/Practica_09_10.cpp 	modified:   hola.cpp

--- hola.cpp
+++ hola.cpp
@@ -1,34 +1,32 @@
 #include <iostream>
 #include <vector>
+#include <string>
 #include <ctime>
 #include <cstdlib>
 using namespace std;
-//matrices
+//estructuras con vectores
+
+struct Estudiante
+{
+    int edad;
+    char nombre[30];
+    char apellido[30];
+    int nota;
+};
 
 int main() {
     srand(time(0));
-    int edades[4][5];
-    for (int i = 0; i < 3; i++) //
-    {
-        for (int j = 0; j < 3; j++) //columnas
-        {
-            
-            if (i==j)
-            {
-                edades[i][j]=-10;
-            }
-            else
-            {
-                edades[i][j]=(rand() % 100)+1;
-            }
-            
-            
-            cout<<edades[i][j]<<" \t";
-
-        }
-        cout<<endl;
-    }
-    
+    Estudiante estudiante1;
+    cout<<"Ingrese su nombre: ";
+    cin.getline(estudiante1.nombre,30);
+    cout<<"Ingrese su apellido: ";
+    cin.getline(estudiante1.apellido,30);
+    cout<<"Ingrese su edad: ";
+    cin>>estudiante1.edad;
+    estudiante1.nota= (rand()% 41)+60;
+    cout<<" Nombre: "<<estudiante1.nombre
+    <<" Apellido: "<<estudiante1.apellido<<endl
+    <<" edad: "<<estudiante1.edad<<" Nota: "<<estudiante1.nota;
     return 0;
 }
 
