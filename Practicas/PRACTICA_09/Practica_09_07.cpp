356a2754924f8d270babad6928cf44a6e5b8357a
jhoelgallegos-Imu <jhoel.gallegos@ucb.edu.bo>
2025-11-02 17:14:03 -0400

    modified:   Practicas/PRACTICA_09/Practica_09_01.cpp 	modified:   Practicas/PRACTICA_09/Practica_09_02.cpp 	modified:   Practicas/PRACTICA_09/Practica_09_03.cpp 	modified:   Practicas/PRACTICA_09/Practica_09_04.cpp 	modified:   Practicas/PRACTICA_09/Practica_09_05.cpp 	modified:   Practicas/PRACTICA_09/Practica_09_06.cpp 	modified:   Practicas/PRACTICA_09/Practica_09_07.cpp 	modified:   Practicas/PRACTICA_09/Practica_09_08.cpp 	modified:   Practicas/PRACTICA_09/Practica_09_09.cpp 	modified:   Practicas/PRACTICA_09/Practica_09_10.cpp

---
+++ Practicas/PRACTICA_09/Practica_09_07.cpp
@@ -0,0 +1,11 @@
+// Materia: Programación I, Paralelo 4
+// Autor: Jhoel Fernando Gallegos Mamani 
+// Fecha creación: 02/11/2025
+// Número de ejercicio: 7
+
+#include <iostream>
+using namespace std;
+
+int main () {
+    return 0;
+}
