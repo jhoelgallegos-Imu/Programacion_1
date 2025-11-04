7b28e1ec669aaf59e6c18e7b11a016aecfefcdc9
jhoelgallegos-Imu <jhoel.gallegos@ucb.edu.bo>
2025-11-04 06:08:33 -0400

    modified:   Practicas/PRACTICA_09/Practica_09_04.cpp 	new file:   Practicas/PRACTICA_09/Practica_09_04.exe 	modified:   Practicas/PRACTICA_09/Practica_09_05.cpp 	new file:   Practicas/PRACTICA_09/Practica_09_05.exe

--- Practicas/PRACTICA_09/Practica_09_04.cpp
+++ Practicas/PRACTICA_09/Practica_09_04.cpp
@@ -1,11 +1,64 @@
 // Materia: Programación I, Paralelo 4
 // Autor: Jhoel Fernando Gallegos Mamani 
-// Fecha creación: 02/11/2025
+// Fecha creación: 04/11/2025
 // Número de ejercicio: 4
 
 #include <iostream>
+#include <vector>
+#include <cstdlib>
 using namespace std;
 
+struct Empleado
+{
+    char nombre[60];
+    char genero[15];
+    float salario;
+};
+
+void MostrarEmpleado(vector<Empleado> & empleados, int nro){ 
+    cout<<"\t Nombre: "<<empleados[nro].nombre<<" | Genero: "<<empleados[nro].genero<<endl
+    <<"\t\t Salario: "<<empleados[nro].salario<<endl;
+}
+
 int main () {
+    int n, MasI, MenosI;
+    float SalMasBajo=21474836 , SalMasAlto=0;
+    vector<Empleado> Empleados;
+    cout<<"Ingrese la cantidad de empleados que quiera ingresar: ";
+    cin>>n;
+    cin.ignore();
+    for (int i = 0; i < n ; i++)
+    {
+        cout<<"=================================================================="<<endl;
+        Empleado EmpleadoX;
+        cout<<"Ingrese el nombre del empleado de la UCB "<<i+1<<" :";
+        cin.getline(EmpleadoX.nombre,60);
+        cout<<"Ingrese el genero del empleado de la UCB "<<i+1<<" :";
+        cin.getline(EmpleadoX.genero,15);
+        cout<<"Ingrese la salario del empleado de la UCB "<<i+1<<" :";
+        cin>>EmpleadoX.salario;
+        if (EmpleadoX.salario > SalMasAlto) //guardo las posiciones y reestablesco los valores del salario mas alto y bajo
+        {
+            MasI=i;
+            SalMasAlto=EmpleadoX.salario;
+        }
+        else if (EmpleadoX.salario < SalMasBajo)
+        {
+            MenosI=i;
+            SalMasBajo=EmpleadoX.salario;
+        }
+        cin.ignore();
+        cout<<"=================================================================="<<endl;
+        Empleados.push_back(EmpleadoX);
+        system("cls");
+    }
+    cout<<"=================================================================="<<endl<<
+    "\t El empleado con mayor salario en la UCB: "<<endl
+    <<"=================================================================="<<endl;
+    MostrarEmpleado(Empleados, MasI);
+    cout<<"=================================================================="<<endl<<
+    "\t El empleado con menor salario en la UCB: "<<endl
+    <<"=================================================================="<<endl;
+    MostrarEmpleado(Empleados, MenosI);
     return 0;
 }
--- Practicas/PRACTICA_09/Practica_09_05.cpp
+++ Practicas/PRACTICA_09/Practica_09_05.cpp
@@ -4,8 +4,81 @@
 // Número de ejercicio: 5
 
 #include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <string>
 using namespace std;
 
+struct Pelicula
+{
+    string titulo;
+    string director;
+    int minutos;
+    int anio_estreno;
+    string genero;
+};
+
+void MostrarPelicula(vector<Pelicula> & pelis, int nro){ 
+    cout<<"\t Titulo: "<<pelis[nro].titulo<<endl
+        <<"\t Director: "<<pelis[nro].director<<" | Genero: "<<pelis[nro].genero<<endl
+        <<"\t Duracion: "<<pelis[nro].minutos<<" | Anio de Estreno: "<<pelis[nro].anio_estreno<<endl;
+}
+
 int main () {
+    int n;
+    string BuscaGenero, BuscaDirector;
+    vector<Pelicula> Peliculas;
+    cout<<"Ingrese la cantidad de Peliculas que quiera ingresar: ";
+    cin>>n;
+    cin.ignore(); 
+    for (int i = 0; i < n ; i++)
+    {
+        cout<<"=================================================================="<<endl;
+        Pelicula PeliculaX;
+        cout<<"Ingrese el Titulo de la Pelicula "<<i+1<<": ";
+        getline(cin, PeliculaX.titulo);
+        cout<<"Ingrese el Director de la Pelicula "<<i+1<<": ";
+        getline(cin, PeliculaX.director);
+        cout<<"Ingrese el genero de la Pelicula "<<i+1<<": ";
+        getline(cin, PeliculaX.genero);
+        cout<<"Ingrese el tiempo (en minutos) de la pelicula "<<i+1<<": ";
+        cin>>PeliculaX.minutos;
+        cout<<"Ingrese el anio de estreno de la pelicula "<<i+1<<": ";
+        cin>>PeliculaX.anio_estreno;
+        cin.ignore(); 
+        cout<<"=================================================================="<<endl;
+        Peliculas.push_back(PeliculaX);
+        system("cls");
+    }
+    cout<<"=================================================================="<<endl
+        <<"\t   Ingrese el genero de peliculas que quiera buscar: "<<endl
+        <<"=================================================================="<<endl;
+    getline(cin, BuscaGenero);
+    for (int i = 0; i < n ; i++)
+    {
+        if (BuscaGenero == Peliculas[i].genero)
+        {
+            cout<<"---------------------------------------------------------"<<endl;
+            MostrarPelicula(Peliculas,i);
+            cout<<"---------------------------------------------------------"<<endl;
+        }
+    }
+    system("pause");
+    system("cls");
+    cout<<"=================================================================="<<endl
+        <<"    Ingrese el director por el que quiera buscar las peliculas: "<<endl
+        <<"=================================================================="<<endl;
+    getline(cin, BuscaDirector);
+    for (int i = 0; i < n ; i++)
+    {
+        if (BuscaDirector == Peliculas[i].director)
+        {
+            cout<<"---------------------------------------------------------"<<endl;
+            MostrarPelicula(Peliculas,i);
+            cout<<"---------------------------------------------------------"<<endl;
+        }
+    }
+    system("pause");
+    system("cls");
     return 0;
 }
