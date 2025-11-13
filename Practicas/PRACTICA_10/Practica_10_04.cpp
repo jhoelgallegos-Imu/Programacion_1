5071f7a8226dc5d5ee817c99c6c2d26aaab69aaa
jhoelgallegos-Imu <jhoel.gallegos@ucb.edu.bo>
2025-11-12 23:07:49 -0400

    modified:   Practicas/PRACTICA_10/Practica_10_04.cpp 	new file:   Practicas/PRACTICA_10/Practica_10_04.exe

--- Practicas/PRACTICA_10/Practica_10_04.cpp
+++ Practicas/PRACTICA_10/Practica_10_04.cpp
@@ -5,9 +5,39 @@
 #include <iostream>
 #include <fstream>
 #include <cstdlib>
-#include <limits>
+#include <string>
 using namespace std;
 
-int main (){
+int Containador(const char nombreArchivo[30], const string &busqueda) {
+    ifstream archivo(nombreArchivo);
+    if (!archivo.is_open()) {
+        cout << "Error: no se pudo abrir el archivo." << endl;
+        return 0;
+    }
+    string linea;
+    int contador = 0;
+    while (getline(archivo, linea)) {
+        size_t pos = linea.find(busqueda);
+        while (pos != string::npos) {
+            contador++;
+            pos = linea.find(busqueda, pos + busqueda.length());
+        }
+    }
+    archivo.close();
+    return contador;
+}
+
+int main() {
+    char nombreArchivo[30] = "ESTUDIANTES.txt"; // use un archivo ya creado para que sea mas facil
+    string busqueda;
+
+    cout << "Ingrese la palabra o frase a buscar: ";
+    getline(cin, busqueda);
+
+    int apariciones = Containador(nombreArchivo, busqueda);
+
+    cout << "La palabra o frase " << busqueda 
+         << " aparece " << apariciones << " veces en el archivo." << endl;
+
     return 0;
 }
